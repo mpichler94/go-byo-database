5cf34e5057b9de0b0448d0000325cd9fbfe66f44
Marcel Pichler <marcel.pichler@ik.me>
2026-02-03 23:00:02 +0100

    0103: implement Log Storage

--- internal/kv.go
+++ internal/kv.go
@@ -3,15 +3,38 @@ package internal
 import "bytes"
 
 type KV struct {
+	log Log
 	mem map[string][]byte
 }
 
 func (kv *KV) Open() error {
-	kv.mem = map[string][]byte{} // empty
+	if err := kv.log.Open(); err != nil {
+		return err
+	}
+
+	kv.mem = map[string][]byte{}
+
+	ent := Entry{}
+	for {
+		eof, err := kv.log.Read(&ent)
+		if eof {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		if ent.deleted {
+			delete(kv.mem, string(ent.key))
+		} else {
+			kv.mem[string(ent.key)] = ent.val
+		}
+	}
+
 	return nil
 }
 
-func (kv *KV) Close() error { return nil }
+func (kv *KV) Close() error { return kv.log.Close() }
 
 func (kv *KV) Get(key []byte) (val []byte, ok bool, err error) {
 	val, ok = kv.mem[string(key)]
@@ -22,12 +45,26 @@ func (kv *KV) Set(key []byte, val []byte) (updated bool, err error) {
 	prev, exist := kv.mem[string(key)]
 	kv.mem[string(key)] = val
 	updated = !exist || !bytes.Equal(prev, val)
+
+	if updated {
+		if err := kv.log.Write(&Entry{key, val, false}); err != nil {
+			return updated, err
+		}
+	}
+
 	return
 }
 
 func (kv *KV) Del(key []byte) (deleted bool, err error) {
 	_, deleted = kv.mem[string(key)]
 	delete(kv.mem, string(key))
+
+	if deleted {
+		if err := kv.log.Write(&Entry{key, nil, true}); err != nil {
+			return deleted, err
+		}
+	}
+
 	return
 }
 
--- internal/kv_entry.go
+++ internal/kv_entry.go
@@ -6,36 +6,45 @@ import (
 )
 
 type Entry struct {
-	key []byte
-	val []byte
+	key     []byte
+	val     []byte
+	deleted bool
 }
 
 func (ent *Entry) Encode() []byte {
-	data := make([]byte, 4+4+len(ent.key)+len(ent.val))
+	data := make([]byte, 4+4+1+len(ent.key)+len(ent.val))
 	binary.LittleEndian.PutUint32(data[0:4], uint32(len(ent.key)))
-	binary.LittleEndian.PutUint32(data[4:8], uint32(len(ent.val)))
-	copy(data[8:], ent.key)
-	copy(data[8+len(ent.key):], ent.val)
-
+	copy(data[9:], ent.key)
+	if ent.deleted {
+		data[8] = 1
+	} else {
+		binary.LittleEndian.PutUint32(data[4:8], uint32(len(ent.val)))
+		copy(data[9+len(ent.key):], ent.val)
+	}
 	return data
 }
 
 func (ent *Entry) Decode(r io.Reader) error {
-	size := make([]byte, 8)
-	if _, err := r.Read(size); err != nil {
+	var header [9]byte
+	if _, err := io.ReadFull(r, header[:]); err != nil {
 		return err
 	}
+	kLen := int(binary.LittleEndian.Uint32(header[0:4]))
+	vLen := int(binary.LittleEndian.Uint32(header[4:8]))
+	deleted := header[8]
 
-	kLen := binary.LittleEndian.Uint32(size[:4])
-	vLen := binary.LittleEndian.Uint32(size[4:])
 	data := make([]byte, kLen+vLen)
-
-	if _, err := r.Read(data); err != nil {
+	if _, err := io.ReadFull(r, data); err != nil {
 		return err
 	}
 
 	ent.key = data[:kLen]
-	ent.val = data[kLen:]
+	if deleted != 0 {
+		ent.deleted = true
+	} else {
+		ent.deleted = false
+		ent.val = data[kLen:]
+	}
 	return nil
 }
 
